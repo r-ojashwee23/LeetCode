8f01e84dca1120c4aa759aa5ddadcaf9af8819ec
Ojashwee Raman <141564615+r-ojashwee23@users.noreply.github.com>
2023-09-20 11:53:51 +0530

    Update Palindrome_Number2.java

--- 9. Palindrome Number/Palindrome_Number2.java
+++ 9. Palindrome Number/Palindrome_Number2.java
@@ -1,1 +1,21 @@
+// Approach: Reversing the string
 
+class Solution {
+    public boolean isPalindrome(int x) {
+        if(x<0){
+            return false;
+        }
+        if(x!=0 && x%10==0){
+            return false;
+        }
+        int rev=0;
+        while(x>rev){
+            rev=rev*10+x%10;
+            x/=10;
+        }
+        if(rev==x || x==rev/10){
+            return true;
+        }
+        return false;
+    }
+}
