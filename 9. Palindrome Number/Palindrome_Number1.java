f4c627c59d151f453ee8c6e5fe822ed61423e5a3
Ojashwee Raman <141564615+r-ojashwee23@users.noreply.github.com>
2023-09-20 11:52:29 +0530

    Update Palindrome_Number1.java

--- 9. Palindrome Number/Palindrome_Number1.java
+++ 9. Palindrome Number/Palindrome_Number1.java
@@ -1,1 +1,16 @@
+// Approach: String matching(6 ms)
 
+class Solution {
+    public boolean isPalindrome(int x) {
+        String s=String.valueOf(x);
+        if(s.charAt(0)=='-'){
+            return false;
+        }
+        for(int i=0; i<s.length()/2; i++){
+            if(s.charAt(i)!=s.charAt(s.length()-1-i)){
+                return false;
+            }
+        }
+        return true;
+    }
+}
